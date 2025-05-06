aa1f43132ff35427b8316800149d89becb1b2424
kurtasov <kurtasov@users.noreply.github.com>
2025-05-06 11:52:51 +0300

    Patching the httpx response

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@ from app.main import read_tasks_for_day
 from fastapi import Response
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 import pytest
+from unittest.mock import patch
 
 
 ASYNC_DB_URL = (f"postgresql+asyncpg://"
@@ -19,9 +20,24 @@ async def get_async_session() -> AsyncSession:
         yield session
 
 
-
-
 @pytest.mark.asyncio
 async def test_main(get_async_session):
-    o = read_tasks_for_day(response=Response(), session=get_async_session)
-    assert await o is not None
+    mock_url = "https://isdayoff.ru/2025-05-06"
+    mock_response_data = "1"
+
+    async def mock_get(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, text_data, status_code):
+                self.text = text_data
+                self.status_code = status_code
+
+            def raise_for_status(self):
+                if self.status_code >= 400:
+                    raise Exception("Request failed")
+
+        return MockResponse(mock_response_data, status_code=200)
+
+    with patch("httpx.AsyncClient.get", new=mock_get):
+        coro = read_tasks_for_day(response=Response(), session=get_async_session)
+        res = await coro
+        assert res[0].get("is_day_off", None) == True
